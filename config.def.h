2e9bea4ee9d25f696606c7fef8b8de530451c9da
justanoobcoder <syaorancode@gmail.com>
2021-06-14 20:20:00 +0700

    config.def.h: patch emoji-highlight

--- config.def.h
+++ config.def.h
@@ -7,14 +7,22 @@ static int min_width = 500;                    /* minimum width when centered */
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "monospace:size=10",
+    "FiraCode Nerd Font:size=12",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+static const char *colors[SchemeLast][10] = {
+    /*     fg         bg       */
+    [SchemeNorm] = { "#bbbbbb", "#222222", "#222222" },
+    [SchemeSel] = { "#eeeeee", "#005577", "#005577" },
+    [SchemeOut] = { "#000000", "#00ffff", "#00ffff" },
+    [SchemeHighlight] = {"#f1fa8c", "#596377", "#3E485B"},
+    [SchemeHover] = {"#ffffff", "#353D4B", "#3E485B"},
+    [SchemeGreen] = {"#ffffff", "#52E067", "#41b252"},
+    [SchemeRed] = {"#ffffff", "#e05252", "#c24343"},
+    [SchemeYellow] = {"#ffffff", "#e0c452", "#bca33f"},
+    [SchemeBlue] = {"#ffffff", "#5280e0", "#3a62b3"},
+    [SchemePurple] = {"#ffffff", "#9952e0", "#7439b0"},
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -25,6 +33,12 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+
+static unsigned int lineheight = 0; /* -h option; minimum height of a menu line     */
+static unsigned int min_lineheight = 8;
+static int sely = 0;
+static int commented = 0;
+static int animated = 1;
 
 /*
  * Characters not considered part of a word while deleting words
