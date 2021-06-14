c90b9f387ae9e2cda0816941bca735e7a69e25c4
justanoobcoder <syaorancode@gmail.com>
2021-06-14 20:46:38 +0700

    config.def.h: patch lineheight

--- config.def.h
+++ config.def.h
@@ -36,9 +36,10 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-
-static unsigned int lineheight = 0; /* -h option; minimum height of a menu line     */
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
+
 static int sely = 0;
 static int commented = 0;
 static int animated = 1;
