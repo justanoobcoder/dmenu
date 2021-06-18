9aa94d9b23eff84c3ffe26cf5fba1e46741b8014
justanoobcoder <syaorancode@gmail.com>
2021-06-18 21:31:55 +0700

    config.def.h: included stddef.h to get rid of NULL error

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#include <stddef.h>
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
